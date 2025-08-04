f0b99f6d7f3d39d7d65146c17c313a5c9e1174dc
Om Shrivastava <omshrivastava01927@gmail.com>
2025-08-04 23:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (56.80%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+from collections import deque
+class Solution(object):
+    def isValid(self, s):
+        stack = deque()
+        for ch in s:
+            if ch in "({[":
+                stack.append(ch)
+            else:
+                if not stack:
+                    return False
+                if ch == ')' and stack[-1] != '(':
+                    return False
+                if ch == '}' and stack[-1] != '{':
+                    return False
+                if ch == ']' and stack[-1] != '[':
+                    return False
+                stack.pop()
+        return len(stack) == 0
